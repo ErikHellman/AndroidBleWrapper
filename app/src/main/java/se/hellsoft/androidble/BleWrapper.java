17fe4069268babe6a83b5d723aafba8245d389a4
Erik Hellman <erik.hellman@hellsoft.se>
2017-09-02 15:15:10 +0200

    Updates for second post.

--- app/src/main/java/se/hellsoft/androidble/BleWrapper.java
+++ app/src/main/java/se/hellsoft/androidble/BleWrapper.java
@@ -8,32 +8,73 @@ import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Looper;
 import android.os.Message;
+import android.support.annotation.MainThread;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public final class BleWrapper implements Handler.Callback {
   private static final int MSG_CONNECT = 10;
   private static final int MSG_CONNECTED = 20;
   private static final int MSG_DISCONNECT = 30;
   private static final int MSG_DISCONNECTED = 40;
-  private static final int MSG_CLOSE = 50;
+  private static final int MSG_SERVICES_DISCOVERED = 50;
 
-  private final BluetoothDevice bluetoothDevice;
+  private BluetoothDevice bluetoothDevice;
+  private Handler mainHandler = new Handler(Looper.getMainLooper(), this);
   private Handler bleHandler;
   private Context context;
   private MyBleCallback myBleCallback = new MyBleCallback();
+
+  private Set<BleCallback> listeners = new HashSet<>();
+
+  public interface BleCallback {
+    /**
+     * Signals that the BLE device is ready for communication.
+     */
+    @MainThread
+    void onDeviceReady();
+
+    /**
+     * Signals that a connection to the device was lost.
+     */
+    @MainThread
+    void onDeviceDisconnected();
+  }
 
   public BleWrapper(Context context, String deviceAddress) {
     this.context = context;
     HandlerThread handlerThread = new HandlerThread("BleThread");
     handlerThread.start();
-    BluetoothManager bluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
-    BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
     bleHandler = new Handler(handlerThread.getLooper(), this);
-    bluetoothDevice = bluetoothAdapter.getRemoteDevice(deviceAddress);
+    bluetoothDevice = getBluetoothDevice(context, deviceAddress);
+  }
+
+  @MainThread
+  public void addListener(BleCallback bleCallback) {
+    listeners.add(bleCallback);
+  }
+
+  @MainThread
+  public void removeListener(BleCallback bleCallback) {
+    listeners.remove(bleCallback);
+  }
+
+  private BluetoothDevice getBluetoothDevice(Context context, String deviceAddress) {
+    BluetoothManager bluetoothManager = (BluetoothManager) context
+        .getSystemService(Context.BLUETOOTH_SERVICE);
+    BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+    return bluetoothAdapter.getRemoteDevice(deviceAddress);
   }
 
   public void connect(boolean autoConncet) {
     bleHandler.obtainMessage(MSG_CONNECT, autoConncet).sendToTarget();
+  }
+
+  public void disconnect() {
+    bleHandler.sendEmptyMessage(MSG_DISCONNECT);
   }
 
   @Override
@@ -43,32 +84,29 @@ public final class BleWrapper implements Handler.Callback {
         doConnect((Boolean) message.obj);
         break;
       case MSG_CONNECTED:
-        doDiscoverServices((BluetoothGatt) message.obj);
+        ((BluetoothGatt) message.obj).discoverServices();
         break;
       case MSG_DISCONNECT:
-        doDisconnect((BluetoothGatt) message.obj);
+        ((BluetoothGatt) message.obj).disconnect();
         break;
       case MSG_DISCONNECTED:
-        doClose((BluetoothGatt) message.obj);
+        ((BluetoothGatt) message.obj).close();
         break;
+      case MSG_SERVICES_DISCOVERED:
+        doNotifyReady();
     }
     return true;
   }
 
+  @MainThread
+  private void doNotifyReady() {
+    for (BleCallback listener : listeners) {
+      listener.onDeviceReady();
+    }
+  }
+
   private void doConnect(boolean autoConnect) {
     bluetoothDevice.connectGatt(context, autoConnect, myBleCallback);
-  }
-
-  private void doClose(BluetoothGatt gatt) {
-    gatt.close();
-  }
-
-  private void doDisconnect(BluetoothGatt gatt) {
-    gatt.disconnect();
-  }
-
-  private void doDiscoverServices(BluetoothGatt gatt) {
-    gatt.discoverServices();
   }
 
   private class MyBleCallback extends BluetoothGattCallback {
@@ -84,6 +122,14 @@ public final class BleWrapper implements Handler.Callback {
             bleHandler.obtainMessage(MSG_DISCONNECTED, gatt).sendToTarget();
             break;
         }
+      }
+    }
+
+    @Override
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+      super.onServicesDiscovered(gatt, status);
+      if (status == BluetoothGatt.GATT_SUCCESS) {
+        mainHandler.sendEmptyMessage(MSG_SERVICES_DISCOVERED);
       }
     }
   }
